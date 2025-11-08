7151705ea183262b1302c33ea601dcc03b738611
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2025-11-08 08:00:46 +0530

    Add files via upload

---
+++ Day2/Conflict_Initializer/Program.cs
@@ -0,0 +1,52 @@
+﻿//namespace Conflict_Initializer
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Console.WriteLine("Hello, World!");
+//            Class1 c1=new Class1();
+//            c1.Prop1 = 1;
+//            c1.Prop2 = 2;
+//            c1.Prop3 = 3;
+//            //Object initializer
+//            //Class1 c2=new Class1()
+//            //{
+//            //    Prop1 = 1,
+//            //    Prop2 = 2,
+//            //    Prop3 = 3
+//            //};
+//            Console.WriteLine(c1.Prop1);
+//            Console.WriteLine(c1.Prop2);
+//            Console.WriteLine(c1.Prop3);
+//        }
+//    }
+
+//    public class Class1
+//    {
+//        private int prop1;
+//        public int Prop1
+//        {
+//            get { return prop1; }
+//            set {  prop1 = value; }
+            
+//        }
+
+//        public int Prop2 { set; get; }
+//        public int Prop3 { set; get; }
+//    }
+//}
+
+
+
+//
+namespace Static_Members
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
