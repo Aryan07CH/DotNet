45c395c1fc45d2a308c52b2be86b25a96a750cf4
Aryan Chogale <128447275+Aryan07CH@users.noreply.github.com>
2025-11-08 07:59:48 +0530

    Add files via upload

---
+++ Day2/AccessSpecifiers/Program.cs
@@ -0,0 +1,37 @@
+﻿﻿namespace AccessSpecifiers
+{
+    //default access specifier is 'internal' in a namespace
+
+    class Program
+    {
+        static void Main()
+        {
+            BaseClass o1 = new BaseClass();
+            //o1.
+
+            TestAccessSpecifier.BaseClass o2 = new TestAccessSpecifier.BaseClass();
+            //o2.
+        }
+    }
+
+    public class BaseClass
+    {
+        int i;
+        //default access specifier is 'private' for class members
+
+        public int PUBLIC;
+        private int PRIVATE;
+        protected int PROTECTED;
+        internal int INTERNAL;
+        protected internal int PROTECTED_INTERNAL;
+        private protected int PRIVATE_PROTECTED;
+    }
+
+    public class DerivedClass  : TestAccessSpecifier.BaseClass   //: BaseClass
+    {
+        void DoNothing()
+        {
+            //this.
+        }
+    }
+}
